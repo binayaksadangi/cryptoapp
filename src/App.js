44f7cdc165ff32d09ae6e311a38a36abaa77e78e
binayaksadangi <youbinayak@gmail.com>
2021-12-21 04:59:59 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from 'react';
 import './App.css';
-import {Router,Route,Link} from 'react-router-dom';
+import {Routes,Route,Link} from 'react-router-dom';
 import {Layout,Typography,Space} from 'antd';
 
-import {Navbar, Home, News, Exchanges, Cryptocurrencies, CryptoDetails} from './components';
+import {Navbar,HomePage,News,Exchanges,Cryptocurrencies,CryptoDetails} from './components';
+
 function App() {
   return (
     <div className="App">
@@ -13,19 +14,27 @@ function App() {
       <div className="main">
         <Layout>
            <div className="routes">
-             <Router>
-               <Route exact path="/" component={Home}/>
-               <Route exact path="/cryptocurrencies" component={Cryptocurrencies}/>
-               <Route exact path="/crypto/:coinId" component={CryptoDetails}/>
-               <Route exact path="/exchanges" component={Exchanges}/>
-               <Route exact path="/news" component={News}/>
-            </Router>
+             <Routes>
+               <Route exact path="/" element={<HomePage/>}/>
+               <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+               <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
+               <Route exact path="/exchanges" element={<Exchanges/>}/>
+               <Route exact path="/news" element={<News/>}/>
+            </Routes>
            </div>
+           
         </Layout>
-      </div>
       <div className="footer">
-
-        
+            <Typography.Title level={5} style={{color: "white", textAlign: 'center'}}>
+              Cryptoverse <br /> 
+              All rights are reserved.
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+      </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import 'antd/dist/antd.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
